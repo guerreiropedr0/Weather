4eb9101acdbd6a1a2c00f229c577dbe517315cb1
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-01-27 19:04:19 +0000

    Add style to CSS

--- src/script.js
+++ src/script.js
@@ -5,9 +5,10 @@ const INPUT_TEXT = document.getElementById('country');
 const DETAILS = document.getElementById('weather-details');
 
 const populate = (weather) => {
-  DETAILS.innerHTML = `<div><h1>${weather.current.temp_c}°C</h1><h2>${weather.location.country}</h2><h3>${weather.location.localtime}</h3><img src="${weather.current.condition.icon}" alt="${weather.current.condition.text}"><h4>${weather.current.condition.text}</h4></div><h5>Weather details</h5><div><p>Cloudy: ${weather.current.cloud}%</p><p>Humidity: ${weather.current.humidity}%</p><p>Wind: ${weather.current.wind_kph}km/h</p></div><div><h5>Next Days</h5></div>`;
+  DETAILS.innerHTML = `<div class="current-weather"><h1>${weather.current.temp_c}°C</h1><div class="country-details"><h2>${weather.location.country}</h2><p>${weather.location.localtime}</p></div><div class="condition-details"><img src="${weather.current.condition.icon}" alt="${weather.current.condition.text}"><p>${weather.current.condition.text}</p></div></div><div class="bro"><div class="test"><h5>Weather details</h5><p>Cloudy: ${weather.current.cloud}%</p><p>Humidity: ${weather.current.humidity}%</p><p>Wind: ${weather.current.wind_kph}km/h</p></div><div class="test2"><h5>Next Days</h5></div></div>`;
+  let test2 = document.querySelector('.test2');
   for (let i = 1; i < 3; i += 1) {
-    DETAILS.innerHTML += `<ul><h6>${weather.forecast.forecastday[i].date}</h6><li>Minimum Temp: ${weather.forecast.forecastday[i].day.mintemp_c}°C</li><li>Avegage Temp: ${weather.forecast.forecastday[i].day.avgtemp_c}°C</li><li>Maximum Temp: ${weather.forecast.forecastday[i].day.maxtemp_c}°C</li></ul>`;
+    test2.innerHTML += `<ul><h6>${weather.forecast.forecastday[i].date}</h6><li>Minimum Temp: ${weather.forecast.forecastday[i].day.mintemp_c}°C</li><li>Avegage Temp: ${weather.forecast.forecastday[i].day.avgtemp_c}°C</li><li>Maximum Temp: ${weather.forecast.forecastday[i].day.maxtemp_c}°C</li></ul>`;
   }
 };
 
@@ -19,7 +20,7 @@ const requestWeather = async (key, country) => {
       headers: {
         'Content-Type': 'application/json',
       },
-    },
+    }
   );
   const DATA = await REQUEST.text();
   const PARSED_DATA = JSON.parse(DATA);
